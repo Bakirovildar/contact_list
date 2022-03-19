3544957e361ac7a0a14e2519bbb3b109e6e0087f
Ildar <bakirovildar2011@mail.ru>
2022-03-19 13:40:37 +0500

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./contacts/header/Header";
+import Main from "./contacts/main/Main"
+import React, {useState} from "react";
 
 function App() {
-  return (
+    const data = {
+        contacts: [
+            {number: '8965584483', name: 'Ivan', id: 1},
+            {number: '8965584124', name: 'Danil', id: 2},
+            {number: '8912484434', name: 'Nikolay', id: 3},
+        ]
+    }
+    const [state, setState] = useState(data)
+    const [showModal, setShowModal] = useState(false)
+    const [valueName, setValueName] =useState('')
+    const [valueNumber, setValueNumber] =useState('')
+
+    const addNewContactHandle = () => {
+        const newContact = {name: valueName, number: valueNumber, id: Date.now()}
+        const newContacts = {contacts:[ newContact, ...state.contacts ]}
+        setState(newContacts)
+        setShowModal(!showModal)
+    }
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header
+            numberValue={(value) => setValueNumber(value)}
+            nameValue={(value) => setValueName(value)}
+            addNewContactHandle={addNewContactHandle}
+            setShowModal={() => setShowModal(!showModal)}
+            showModal={showModal}
+        />
+        <Main contacts={state.contacts}/>
     </div>
   );
 }
---
+++ src/contacts/header/Header.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import './Header.css'
+import MyButton from "../../component/myButton/MyButton";
+import ModalWindow from "../../component/modalWindow/ModalWindow";
+
+const Header = ({showModal, setShowModal, numberValue, nameValue, addNewContactHandle}) => {
+
+    return (
+        <div className='header'>
+            {showModal === true
+                ? <div className='modalWind'>
+                    <ModalWindow
+                        offShowModalWindow={setShowModal}
+                        nameValue={nameValue}
+                        numberValue={numberValue}
+                        addNewContactHandle={addNewContactHandle}
+                    />
+                </div>
+                : ''
+            }
+            <MyButton
+                onClick={setShowModal}
+                cls='add'
+                title='Добавить контакт'
+            />
+        </div>
+    )
+}
+
+export default Header
