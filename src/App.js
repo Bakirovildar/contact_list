c8c621348ba0adbc9f5a50dd8b70eedb41644055
Ildar <bakirovildar2011@mail.ru>
2022-03-20 15:16:24 +0500

    add edit filter

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ function App() {
     const [state, setState] = useState(data)
     const [showModal, setShowModal] = useState(false)
     const [deleteModalWindow, setDeleteModalWindow] = useState(false)
-    const [isNew, setIsNew] = useState(true)
-    const [valueName, setValueName] =useState('')
-    const [valueNumber, setValueNumber] =useState('')
+    const [isNew, setIsNew] = useState(false)
+    const [valueName, setValueName] = useState('')
+    const [valueNumber, setValueNumber] = useState('')
+    const [editItem, setEditItem] = useState('')
 
     const addNewContactHandle = () => {
         const newContact = {name: valueName, number: valueNumber, id: Date.now()}
-        const newContacts = {contacts:[ newContact, ...state.contacts ]}
+        const newContacts = {contacts: [newContact, ...state.contacts]}
         setState(newContacts)
         setShowModal(!showModal)
     }
@@ -35,34 +36,52 @@ function App() {
 
     const addContactShowModal = () => {
         setShowModal(!showModal)
-        setIsNew(false)
+        setIsNew(true)
     }
 
-    const showEditModalWindow = (id) => {
+    const showEditModalWindow = (contact) => {
         setShowModal(!showModal)
-        setIsNew(true)
+        setIsNew(false)
+        setEditItem(contact)
+    }
 
+    const editContactHandler = (editItem) => {
+        const newState = {...state}
+        const indexContact = state.contacts.indexOf(editItem)
+        const newContact = state.contacts[indexContact] = {number: valueNumber, name: valueName, id: Date.now()}
+        newState.contacts.splice(indexContact, 1, newContact)
+        setState(newState)
+        setShowModal(false)
+    }
+
+    const clickOkHandler = (isNew, number, name, editItem) => {
+        if (isNew) {
+            addNewContactHandle()
+        } else {
+            editContactHandler(editItem)
+        }
     }
 
     return (
-    <div className="App">
-        <Header
-            numberValue={(value) => setValueNumber(value)}
-            nameValue={(value) => setValueName(value)}
-            addNewContactHandle={addNewContactHandle}
-            setShowModal={addContactShowModal}
-            isNew={isNew}
-            showModal={showModal}
-        />
-        <Main
-            contacts={state.contacts}
-            deleteContactHandle={deleteContactHandle}
-            setDeleteModalWindow={() => setDeleteModalWindow(!deleteModalWindow)}
-            deleteModalWindow={deleteModalWindow}
-            showEditModalWindow={showEditModalWindow}
-        />
-    </div>
-  );
+        <div className="App">
+            <Header
+                numberValue={(value) => setValueNumber(value)}
+                nameValue={(value) => setValueName(value)}
+                addNewContactHandle={clickOkHandler}
+                setShowModal={addContactShowModal}
+                isNew={isNew}
+                showModal={showModal}
+                editItem={editItem}
+            />
+            <Main
+                contacts={state.contacts}
+                deleteContactHandle={deleteContactHandle}
+                setDeleteModalWindow={() => setDeleteModalWindow(!deleteModalWindow)}
+                deleteModalWindow={deleteModalWindow}
+                showEditModalWindow={showEditModalWindow}
+            />
+        </div>
+    );
 }
 
 export default App;
