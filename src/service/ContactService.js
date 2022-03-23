f2da1c6c371de9c43771b14e73304df246518339
Ildar <bakirovildar2011@mail.ru>
2022-03-23 10:46:38 +0500

    add ContactService.js

---
+++ src/service/ContactService.js
@@ -0,0 +1,42 @@
+const getAllContacts = () => {
+    return fetch('http://localhost:8080/contacts')
+        .then(data => data.json())
+        .then(json => json.map(contact => ({...contact, id: contact._id})))
+}
+
+const deleteContact = id => {
+    fetch(`http://localhost:8080/contacts/${id}`, {
+        method: 'DELETE'
+    }).then(r => {
+        console.log(`successfully deleted contact with id = ${id}`)
+    })
+}
+
+const addContact = contact => {
+    console.log(JSON.stringify(contact))
+    fetch('http://localhost:8080/contacts', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    }).then(r => {
+        console.log(`successfully added contact ${contact}`)
+    })
+}
+
+const updateContact = contact => {
+    fetch(`http://localhost:8081/contacts/${contact.id}`, {
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(contact)
+    }).then(r => {
+        console.log(`successfully updated contact ${contact}`)
+    })
+}
+
+export {getAllContacts, deleteContact, addContact, updateContact}
